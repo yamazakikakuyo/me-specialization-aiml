b2986cd9b1f10b16d425a324389fdedaa8aa667a
yamazakikakuyo <38454914+yamazakikakuyo@users.noreply.github.com>
2024-06-10 10:23:11 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ import pandas as pd
 import numpy as np
 import functions as fc
 import time
+import matplotlib.pyplot as plt
 
 st.set_page_config(
-    page_title="SQL Talk with BigQuery",
+    page_title="Stock Prediction AutoML",
     page_icon="metrodata-icon.png",
     layout="wide",
 )
@@ -26,6 +27,18 @@ st.markdown(
     )
 
 # Initialize session state keys if they don't exist
+
+if "result" not in st.session_state:
+    st.session_state.result = None
+
+if "result_type" not in st.session_state:
+    st.session_state.result_type = None
+
+if "list_items" not in st.session_state:
+    st.session_state.list_items = None
+
+if "real_value" not in st.session_state:
+    st.session_state.real_value = None
 
 # Function to reset the success message and related state variables
 def reset_state():
@@ -54,19 +67,50 @@ selected = option_menu(None, ["Prediction", "Result"],
 )
 
 if selected == "Prediction":
+    pred_option = st.selectbox(
+        "How long you want to predict?",
+        ("1 Month", "3 Months")
+    )
     generate_predict = st.button("Generate Prediction")
-    if generate_predict:
-        with st.spinner("Please wait around 10-15 minutes due many data"):   
-            st.session_state.result = fc.run_prediction()
+    if generate_predict: 
+        st.session_state.result = fc.run_prediction(pred_option)
+    st.table(fc.get_list_batch_prediction_job())
 
 if selected == "Result":
+    result_option = st.selectbox(
+        "How long you want to predict?",
+        ("1 Month", "3 Months")
+    )
     prediction_result = st.button("Show Prediction Result")
     if prediction_result:
-        st.write("5 Sample Data Bulan Desember 2016 untuk semua Produk")
-        result = fc.get_predicted_data()
-        st.table(result.head(5))
+        st.session_state.result = fc.get_predicted_data(result_option)
+        st.session_state.result_type = result_option
+        st.session_state.list_items = list(set(st.session_state.result['Product_Code'].values))
+        st.session_state.real_value = fc.get_real_data(result_option)
+    
+    if type(st.session_state.result) != type(None):
+        st.write(f"5 Sample Prediction for {st.session_state.result_type}")
+        st.table(st.session_state.result.head(5))
+        pred_option = st.selectbox(
+            "How long you want to predict?",
+            st.session_state.list_items
+        )
+        generate_graph = st.button("Generate Graph")
         st.download_button(
                     "Download Prediction Data",
-                    result.to_csv(index=False).encode("utf-8"), 
+                    st.session_state.result.to_csv(index=False).encode("utf-8"), 
                     file_name=f"prediction_result.csv",
                     mime='text/csv')
+        if generate_graph:
+            real_data = st.session_state.real_value[st.session_state.real_value['Product_Code'] == pred_option].sort_values(by=["Date"]).dropna()
+            pred_data = st.session_state.result[st.session_state.result['Product_Code'] == pred_option].sort_values(by=["Date"])
+            plt.figure(figsize=(20, 5))
+            plt.plot(pd.concat([real_data['Date'],pred_data['Date']]), pd.concat([real_data['Order_Demand'], pred_data['Predicted_Order_Demand']]), label='Predicted Data', color='orange')
+            plt.plot(real_data['Date'], real_data['Order_Demand'], label='Real Data', color='blue')
+            print(real_data)
+            print(pred_data)
+            plt.xlabel('Month')
+            plt.ylabel('Sales')
+            plt.title(f'Prediction of {pred_option} for {st.session_state.result_type}')
+            plt.legend()
+            st.pyplot(plt)
--- functions.py
+++ functions.py
@@ -5,17 +5,15 @@ from google.protobuf.struct_pb2 import Value
 import pandas as pd
 import numpy as np
 import json
+from datetime import datetime
 
 PROJECT_ID = "me-specialization-aiml"
-TABLE_FORECAST_NAME = "one_month"
+LOCATION_ID = "asia-southeast2"
 
-### I use this when developing in local, it faster than developing in cloud run. So, comment these lines when deploying ###
-client_bigquery = bigquery.Client(project=PROJECT_ID, location='asia-southeast2')
-aiplatform.init(project=PROJECT_ID, location='asia-southeast2')
-
-### Instead, use these lines when deploying in Cloud Run ###
-
-# client_bigquery = bigquery.Client(project=PROJECT_ID, location='asia-southeast2',)
+client_bigquery = bigquery.Client(project=PROJECT_ID, location=LOCATION_ID)
+aiplatform.init(project=PROJECT_ID, location=LOCATION_ID)
+client_options = {"api_endpoint": f"{LOCATION_ID}-aiplatform.googleapis.com"}
+client_jobservice = aiplatform.gapic.JobServiceClient(client_options=client_options)
 
 def batch_prediction(
     project: str,
@@ -23,8 +21,7 @@ def batch_prediction(
     model_resource_name: str,
     job_display_name: str,
     bigquery_source: str,
-    bigquery_destination_prefix: str, #dia otomatis ngebuat tabel baru, cukup masukan project dan dataset. contoh : "me-specialization-aiml.demand_batch_forecast"
-    sync: bool = True,
+    bigquery_destination_prefix: str,
 ):
     my_model = aiplatform.Model(model_resource_name)
 
@@ -32,22 +29,21 @@ def batch_prediction(
         job_display_name=job_display_name,
         bigquery_source=bigquery_source,
         bigquery_destination_prefix=bigquery_destination_prefix,
-        sync=sync,
+        sync=False,
     )
-
-    batch_prediction_job.wait()
-
-    print(batch_prediction_job.display_name)
-    print(batch_prediction_job.resource_name)
-    print(batch_prediction_job.state)
-    print(batch_prediction_job.output_info)
+    batch_prediction_job.wait_for_resource_creation()
     return batch_prediction_job
 
-def get_predicted_data():
+def get_predicted_data(result_option):
+    table_name = ''
+    if result_option == "1 Month":
+        table_name = "predictions_2024_06_07T17_56_28_931Z_039"
+    else:
+        table_name = "predictions_2024_06_08T05_34_44_747Z_462"
     query = """
     SELECT Product_Code, Month, Date, predicted_Order_Demand.value
-    FROM `me-specialization-aiml.demand_batch_forecast.predictions_2024_06_07T17_56_28_931Z_039`
-    """
+    FROM `me-specialization-aiml.demand_batch_forecast.{}`
+    """.format(table_name)
 
     # Menjalankan query dan mengambil hasil
     query_job = client_bigquery.query(query)
@@ -61,16 +57,74 @@ def get_predicted_data():
     results = results.drop(columns=['value'])
     return results
 
-def run_prediction():
-    obj_batch_pred = batch_prediction(
-        project=PROJECT_ID,
-        location="asia-southeast2",
-        model_resource_name="577146847559155712",
-        job_display_name="monthly_demand_forecast",
-        bigquery_source="bq://me-specialization-aiml.demand_dataset.dataset_testing",
-        bigquery_destination_prefix="bq://me-specialization-aiml.demand_batch_forecast",
+def get_real_data(result_option):
+    table_name = ''
+    if result_option == "1 Month":
+        table_name = "dataset_testing"
+    else:
+        table_name = "dataset_testing_threemonth"
+    query = """
+    SELECT Product_Code, Month, Date, Order_Demand
+    FROM `me-specialization-aiml.demand_dataset.{}`
+    """.format(table_name)
+
+    # Menjalankan query dan mengambil hasil
+    query_job = client_bigquery.query(query)
+    results = query_job.result().to_dataframe()
+
+    # Mengonversi kolom Date menjadi datetime dan menghilangkan waktu
+    results['Date'] = pd.to_datetime(results['Date']).dt.date
+    
+    return results
+
+def run_prediction(pred_option):
+    if pred_option == "1 Month":
+        obj_batch_pred = batch_prediction(
+            project=PROJECT_ID,
+            location=LOCATION_ID,
+            model_resource_name="577146847559155712",
+            job_display_name="monthly_demand_forecast_1_month",
+            bigquery_source="bq://me-specialization-aiml.demand_dataset.dataset_testing",
+            bigquery_destination_prefix="bq://me-specialization-aiml.demand_batch_forecast",
+        )
+    else:
+        obj_batch_pred = batch_prediction(
+            project=PROJECT_ID,
+            location=LOCATION_ID,
+            model_resource_name="2675542798937096192",
+            job_display_name="monthly_demand_forecast_3_months",
+            bigquery_source="bq://me-specialization-aiml.demand_dataset.dataset_testing_threemonth",
+            bigquery_destination_prefix="bq://me-specialization-aiml.demand_batch_forecast",
+        )
+
+def get_list_batch_prediction_job():
+    request = aiplatform.gapic.ListBatchPredictionJobsRequest(
+        parent=f"projects/{PROJECT_ID}/locations/{LOCATION_ID}",
     )
+    page_result = client_jobservice.list_batch_prediction_jobs(request=request)
 
-    result = get_predicted_data()
+    state_name = {
+        "JOB_STATE_UNSPECIFIED": "Unspecified",
+        "JOB_STATE_QUEUED": "Just created or resumed and processing has not yet begun.",
+        "JOB_STATE_PENDING": "Preparing to run prediction.",
+        "JOB_STATE_RUNNING": "Prediction is on progress.",
+        "JOB_STATE_SUCCEEDED": "Prediction completed successfully.",
+        "JOB_STATE_FAILED": "Predicition job failed.",
+        "JOB_STATE_CANCELLING": "Prediction is on progress.",
+        "JOB_STATE_CANCELLED": "Prediction has been cancelled.",
+        "JOB_STATE_PAUSED": "Prediction has been stopped, and can be resumed.",
+        "JOB_STATE_EXPIRED": "Prediction has expired.",
+        "JOB_STATE_UPDATING": "Prediction is being updated",
+        "JOB_STATE_PARTIALLY_SUCCEEDED": "Prediction is partially succeeded, some results may be missing due to errors."}
 
-    return result
+    result = []
+    for x in page_result:
+        result.append({
+            "Creation Time": datetime.fromtimestamp(int(x.create_time.timestamp())).strftime("%A, %d %B %Y, %I:%M:%S"),
+            "Name": x.display_name, 
+            "Input": x.input_config.bigquery_source.input_uri, 
+            "Output": x.output_info.bigquery_output_dataset + '.' + x.output_info.bigquery_output_table,
+            "State": state_name[x.state.name]
+        })
+
+    return pd.DataFrame(result, columns=["Creation Time", "Name", "Input", "Output", "State"])
